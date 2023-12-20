5b2d4d869ee8030e3b2b2173b623b34ce2dea986
Jyotsna-Akkinapalli <40540136+Jyotsna-Akkinapalli@users.noreply.github.com>
2023-12-20 11:37:39 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:create_user_profile/Login.dart';
+import 'package:lab10/Login.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
